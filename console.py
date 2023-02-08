831137bf80e32ae254971ae6595a0b02800fc1be
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-02-08 13:03:53 +0000

    The console: Aded quit, EOF, with empty line and hbnb custom promt

--- console.py
+++ console.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+"""The concolse"""
+
+
 import cmd
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, *args):
+        return True
+
+    def do_EOF(self, *args):
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
